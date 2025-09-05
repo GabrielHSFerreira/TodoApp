40ae77a165e4c9bf52f409430e55ec0b165026ca
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2025-09-05 13:32:02 -0300

    Checkpoint.

--- src/TodoApp.AppHost/Program.cs
+++ src/TodoApp.AppHost/Program.cs
@@ -1,10 +1,25 @@
-var builder = DistributedApplication.CreateBuilder(args);
+namespace TodoApp.AppHost;
 
-var postgres = builder.AddPostgres("postgres");
-var postgresDb = postgres.AddDatabase("Database", databaseName: "todoapp-db");
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.TodoApp_WebApi>("todoapp-webapi")
-    .WithReference(postgresDb)
-    .WaitFor(postgresDb);
+        var postgres = builder.AddPostgres("postgres");
+        var postgresDb = postgres.AddDatabase("Database", databaseName: "todoapp-db");
 
-await builder.Build().RunAsync();
+        var username = builder.AddParameter("username", "local");
+        var password = builder.AddParameter("password", "local");
+        var rabbitmq = builder.AddRabbitMQ("RabbitMQ", username, password)
+            .WithManagementPlugin();
+
+        builder.AddProject<Projects.TodoApp_WebApi>("todoapp-webapi")
+            .WithReference(postgresDb)
+            .WithReference(rabbitmq)
+            .WaitFor(postgresDb)
+            .WaitFor(rabbitmq);
+
+        await builder.Build().RunAsync();
+    }
+}
