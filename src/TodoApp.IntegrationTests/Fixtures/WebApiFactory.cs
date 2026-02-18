85d453faca0794ba7980ac77cad425cea54a32ae
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2026-02-18 15:51:04 -0300

    Merge again database and API test fixtures.

--- src/TodoApp.IntegrationTests/Fixtures/WebApiFactory.cs
+++ src/TodoApp.IntegrationTests/Fixtures/WebApiFactory.cs
@@ -1,28 +1,45 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using TodoApp.IntegrationTests.Fixtures;
 using TodoApp.WebApi;
 using TodoApp.WebApi.Contexts;
 
+[assembly: AssemblyFixture(typeof(WebApiFactory))]
+
 namespace TodoApp.IntegrationTests.Fixtures
 {
-    public class WebApiFactory : WebApplicationFactory<Program>
+    public class WebApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
         public TodoContext Context => Services
             .CreateScope()
             .ServiceProvider
             .GetRequiredService<TodoContext>();
 
-        private readonly PostgreSqlDatabase _database;
+        private readonly PostgreSqlContainer _postgreSqlContainer;
 
-        public WebApiFactory(PostgreSqlDatabase postgreSqlContainer)
+        public WebApiFactory()
         {
-            _database = postgreSqlContainer ?? throw new ArgumentNullException(nameof(postgreSqlContainer));
+            _postgreSqlContainer = new PostgreSqlBuilder("postgres:18.1").Build();
         }
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseSetting("ConnectionStrings:Database", _database.Container.GetConnectionString());
+            builder.UseSetting("ConnectionStrings:Database", _postgreSqlContainer.GetConnectionString());
+        }
+
+        public async ValueTask InitializeAsync()
+        {
+            await _postgreSqlContainer.StartAsync();
+            await Context.Database.EnsureCreatedAsync();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await _postgreSqlContainer.DisposeAsync();
+            await base.DisposeAsync();
+            GC.SuppressFinalize(this);
         }
     }
 }
